436675008367446f339349e089c3a5026046f258
Andre <andreferreira@live.co.uk>
2023-01-07 23:42:38 +0000

    post requests

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -4,17 +4,18 @@
 <div class="wrapper create-pizza">
     <h1>Create a new pizza</h1>
     <form action="/pizzas" method="POST">
+        @csrf
         <label for="name">Your name</label>
         <input type="text" id="name" name="name">
         <label for="type">Choose pizza type:</label>
-        <select type="text" id="type">
+        <select name="type" id="type">
             <option value="Simple">Simple</option>
             <option value="Vegetarian">Vegetarian</option>
             <option value="Meat">Meat</option>
             <option value="Spicy">Spicy</option>
         </select>
         <label for="base">Choose base type:</label>
-        <select type="base" id="base">
+        <select name="base" id="base">
             <option value="Thin">Thin</option>
             <option value="Normal">Normal</option>
             <option value="Cheese">Cheese</option>
