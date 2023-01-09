4c34f5a63ad0b657e4842f06ed2c9b0f22f61717
Andre <andreferreira@live.co.uk>
2023-01-09 23:29:33 +0000

    arrays and json - casts for db

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -21,6 +21,14 @@
             <option value="Cheese">Cheese</option>
             <option value="Thick">Thick</option>
         </select>
+        <fieldset>
+            <label>Extra toppings:</label><br />
+            <input type="checkbox" name="toppings[]" value="garlic">Garlic<br />
+            <input type="checkbox" name="toppings[]" value="mushrooms">Mushrooms<br />
+            <input type="checkbox" name="toppings[]" value="peppers">Peppers<br />
+            <input type="checkbox" name="toppings[]" value="onions">Onions<br />
+            <input type="checkbox" name="toppings[]" value="olives">Olives<br />
+        </fieldset>
         <input type="submit" value="order pizza">
     </form>
 </div>
--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -5,6 +5,12 @@
     <h1>Pizza ordered by {{ $pizza->name }}</h1>
     <p>type: {{ $pizza->type }}</p>
     <p>base: {{ $pizza->base }}</p>
+    <p class="toppings">extra toppings:</p>
+    <ul>
+        @foreach($pizza->toppings as $topping)
+        <li>{{ $topping }}</li>
+        @endforeach
+    </ul>
 </div>
 <a href="/pizzas" class="back">
     <-- back to pizzas</a>
